836c48a90d3e35e58370cd402a09aefe91d582b6
Skandalabrandur <steinerinn@gmail.com>
2020-10-29 16:08:04 +0000

    Now, do changes get reflected instantly on the remote server?

--- server.rb
+++ server.rb
@@ -4,6 +4,6 @@ get '/?' do
     return "Yea, I work out. What about you?"
 end
 
-get '/echo/?' do
-    return "ECHO! " * Random.rand 1..10
+get '/marko/?' do
+    return "Polo! " * Random.rand(1..10)
 end
