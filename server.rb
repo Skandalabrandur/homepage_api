7a4b0470846712bab617ee4ae4232f4959248ce5
Skandalabrandur <steinerinn@gmail.com>
2020-10-29 18:13:47 +0000

    Testing dummy api functions over to the frontend

--- server.rb
+++ server.rb
@@ -1,9 +1,14 @@
 require 'sinatra'
+require 'json'
+
+before do
+    content_type :json
+end
 
 get '/?' do
     return "Yea, I work out. What about you?"
 end
 
-get '/marko/?' do
-    return "Polo! " * Random.rand(1..10)
+get '/rhymes/?' do
+    return ["Funky", "Junkie", "Spunky", "Spelunky", "Bunkie", "Hunkie", "Lunky", "Blunky", "Munky", "Runky"].to_json
 end
